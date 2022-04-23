8726098dd1871990aa7a9304eb4ea4093c089c03
patostickar <pstickar@gmail.com>
2022-04-23 09:31:33 -0300

    populate DB w/temperaments from API

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
-const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-
+const { Router } = require("express");
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+const dog = require("./dog.js");
+const dogs = require("./dogs.js");
+const temperament = require("./temperament.js");
 
+router.use("/dog", dog);
+router.use("/dogs", dogs);
+router.use("/temperament", temperament);
 
 module.exports = router;
